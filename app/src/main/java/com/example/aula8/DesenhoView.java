70c40db1b052c867c6528176a822f606e204db14
Nicolas <nicolas.rodrigues5364@gmail.com>
2022-09-28 21:27:21 -0300

    first commit

---
+++ app/src/main/java/com/example/aula8/DesenhoView.java
@@ -0,0 +1,94 @@
+package com.example.aula8;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.transition.Slide;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DesenhoView extends View {
+    private static final float TOUCH_TOLERANCE = 10;
+    private Bitmap bitmap;
+    private Canvas bitmapCanvas;
+    private final Paint paintScreen = new Paint();
+    private final Paint paintLine = new Paint();
+    private final Map<Integer, Path> pathMap  = new HashMap<>();
+    private final Map<Integer, Point> previousPointMap  = new HashMap<>();
+    public DesenhoView(Context context, @Nullable  AttributeSet attrs){
+        super(context, attrs);
+        paintLine.setAntiAlias(true);
+        paintLine.setColor(Color.RED);
+        paintLine.setStyle(Paint.Style.STROKE);
+        paintLine.setStrokeCap(Paint.Cap.ROUND);
+
+    }
+    @Override
+    public void onSizeChanged(int w, int h, int oldW, int oldH){
+        bitmap = Bitmap.createBitmap(getWidth(),getHeight(), Bitmap.Config.ARGB_8888);
+        bitmapCanvas = new Canvas(bitmap);
+        bitmap.eraseColor((Color.WHITE));
+    }
+    public void clear(){
+        pathMap.clear();
+        previousPointMap.clear();
+        bitmap.eraseColor(Color.WHITE);
+        invalidate();
+    }
+    @Override
+    protected  void onDraw (Canvas canvas){
+        canvas.drawBitmap(bitmap, 0,0,paintScreen);
+        for (Integer key: pathMap.keySet()){
+            canvas.drawPath(pathMap.get(key),paintLine);
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        int action = event.getActionMasked();
+        int actionIndex = event.getActionIndex();
+        if(action==MotionEvent.ACTION_DOWN||action==MotionEvent.ACTION_POINTER_DOWN){
+            touchStarted(event.getX(actionIndex),event.getY(actionIndex),
+                    event.getPointerId(actionIndex));
+
+        } else if (action==MotionEvent.ACTION_UP)||action==MotionEvent.ACTION_POINTER_UP){
+    touchEnded(event.getPointerId(actionIndex));
+        } else{
+    touchMoved(event);
+
+        }
+        invalidate();
+        return true;
+    }
+    private
+    void touchStarted(float x, float y, int lineId) {
+        Path path;
+        Point point;
+        if(pathMap.containsKey(lineId)){
+            path = pathMap.get(lineId);
+            path.reset();
+            point = previousPointMap.get(lineId);
+        }else{
+            path = new Path();
+            pathMap.put(lineId,path);
+            point = new Point();
+            previousPointMap.put(lineId,point);
+
+        }
+        path.moveTo(x,y);
+        point.x = (int)x;
+        point.y = (int)y;
+
+    }
+
+
+}
